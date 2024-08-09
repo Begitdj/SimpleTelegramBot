c97b2d367db7b60a02489090e4f8a6feb1e245f0
Begitdj <egyptha29@gmail.com>
2024-08-09 11:53:53 +0700

    Update SimpleBot.py

--- SimpleBot.py
+++ SimpleBot.py
@@ -6,9 +6,6 @@ bot = telebot.TeleBot(tokenx)
 def start(m, res=False):
     bot.send_message(m.chat.id, 'Hello! Send some message')
     print("Some user used /start")
-@bot.message_handler(commands=["stop"])
-def start(m, res=False):
-    bot.died("yes and developer")
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     bot.send_message(message.chat.id, message.text)
